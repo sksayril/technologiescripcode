439abb3c42d8ee5db842851c0fa8941346d08322
sksayril <sksayril123@gmail.com>
2025-07-29 10:22:31 +0530

    pricing module

--- .next/static/chunks/app/pricing/page-c9dfcc2d40c8ebaa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[979],{4667:function(n,e,u){Promise.resolve().then(u.bind(u,8206))}},function(n){n.O(0,[828,985,661,206,971,864,744],function(){return n(n.s=4667)}),_N_E=n.O()}]);
